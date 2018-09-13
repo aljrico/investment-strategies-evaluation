6de84aa60da1141981258cf0828698eeef816a6f
Alejandro Jiménez <aljrico@gmail.com>
2018-09-13 17:47:39 +0100

    tidy separated files and more indexes

---
+++ get_tickers.R
@@ -0,0 +1,54 @@
+getTickers <- function(index = "sp500"){
+
+	if(index == "sp500"){
+		sp500_wiki <- read_html(
+			"https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+
+		symbols_table <- sp500_wiki %>%
+			html_nodes(xpath='//*[@id="mw-content-text"]/div/table[1]') %>%
+			html_table()
+
+		symbols_table <- symbols_table[[1]]
+		tickers <- as.character(symbols_table$`Ticker symbol`)
+	}
+
+	if(index == "sp1000"){
+		sp1000_wiki <- read_html(
+			"https://en.wikipedia.org/wiki/List_of_S%26P_1000_companies")
+
+		symbols_table <- sp1000_wiki %>%
+			html_nodes(xpath='//*[@id="mw-content-text"]/div/table[2]') %>%
+			html_table()
+
+		symbols_table <- symbols_table[[1]]
+		tickers <- as.character(symbols_table$`Ticker Symbol`)
+	}
+
+	if(index == "all"){
+		sp500_wiki <- read_html(
+			"https://en.wikipedia.org/wiki/List_of_S%26P_500_companies")
+
+		symbols_table <- sp500_wiki %>%
+			html_nodes(xpath='//*[@id="mw-content-text"]/div/table[1]') %>%
+			html_table()
+
+		symbols_table <- symbols_table[[1]]
+		tickers <- as.character(symbols_table$`Ticker symbol`)
+
+		sp1000_wiki <- read_html(
+			"https://en.wikipedia.org/wiki/List_of_S%26P_1000_companies")
+
+		symbols_table <- sp1000_wiki %>%
+			html_nodes(xpath='//*[@id="mw-content-text"]/div/table[2]') %>%
+			html_table()
+
+		symbols_table <- symbols_table[[1]]
+		tickers2 <- as.character(symbols_table$`Ticker Symbol`)
+
+		tickers <- c(tickers,tickers2)
+	}
+
+	for(i in 1:length(tickers)) tickers[[i]] <- gsub('\\.', '-', tickers[[i]])
+
+	return(tickers)
+}
---
+++ main.R
@@ -0,0 +1,67 @@
+library(tidyverse)
+library(data.table)
+library(quantmod)
+library(rvest)
+library(progress)
+library(lubridate)
+
+source("get_financials.R")
+source("get_tickers.R")
+
+
+# Retrieve Data -----------------------------------------------------------
+
+tickers <- getTickers(index = "all")
+fins <- getFinancials(tickers)
+
+
+# Tidy Data ---------------------------------------------------------------
+
+# Prices
+all_prices <- tibble()
+
+for(i in 1:length(tickers)){
+	raw_df <- stock_prices[[tickers[[i]]]][,6] %>% data.frame()
+	dates <- raw_df %>% row.names() %>% as.Date()
+	prices <- raw_df[,1]
+
+	new_df <- data.frame(price = as.numeric(prices), date = dates) %>%
+		data.table()
+
+	new_df$firm <- tickers[[i]]
+
+	all_prices <- rbind(all_prices,new_df)
+}
+
+
+# Financials
+fins[is.na(fins)] <- 0
+
+
+
+# Plots -------------------------------------------------------------------
+
+fins %>%
+	ggplot(aes(colour = company)) +
+	geom_point(aes(x = date, y = as.numeric(TotalRevenue)/as.numeric(LongTermDebt))) +
+	theme_bw()
+
+fins %>%
+	ggplot(aes(colour = company)) +
+	geom_point(aes(x = date, y = as.numeric(LongTermInvestments)/as.numeric(TotalRevenue))) +
+	geom_line(aes(x = date, y = as.numeric(LongTermInvestments)/as.numeric(TotalRevenue))) +
+	theme_minimal()
+
+
+# Filtering ---------------------------------------------------------------
+
+fins %>%
+	mutate(invest_rev_ratio = as.numeric(LongTermInvestments)/as.numeric(TotalRevenue)) %>%
+	top_n(20, as.numeric(IncomeBeforeTax)) %>%
+	ggplot(aes(colour = company)) +
+	geom_point(aes(x = date, y = as.numeric(LongTermInvestments)/as.numeric(TotalRevenue))) +
+	geom_line(aes(x = date, y = as.numeric(LongTermInvestments)/as.numeric(TotalRevenue))) +
+	theme_minimal()
+
+
+
